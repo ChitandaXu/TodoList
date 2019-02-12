3ea87b843f7af268b6e1f980c46bc98413ca800b
ChitandaXu <xuxuexiang@smail.nju.edu.cn>
2019-02-12 12:33:42 +0800

    using saga

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import store from './store';
-import { getInputChangeAction, getAddItemAction, getDeleteItemAction, getTodoList } from './store/actionCreators';
+import { getInputChangeAction, getAddItemAction, getDeleteItemAction, getInitList } from './store/actionCreators';
 import TodoListUI from './TodoListUI';
 
 class TodoList extends Component {
@@ -28,7 +28,7 @@ class TodoList extends Component {
   }
 
   componentDidMount() {
-    const action = getTodoList();
+    const action = getInitList();
     store.dispatch(action);
   }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -12,13 +12,11 @@ export default (state = defaultState, action) => {
     newState.inputValue = action.value;
     return newState;
   }
-
   if (action.type === INIT_LIST_ACTION) {
     const newState = JSON.parse(JSON.stringify(state));
     newState.list = action.data;
     return newState;
   }
-
   if (action.type === ADD_TODO_ITEM) {
     const newState = JSON.parse(JSON.stringify(state));
     newState.list.push(newState.inputValue);
