8cd92ec508e0dea462f9e83ea84cda173e418c7c
ChitandaXu <xuxuexiang@smail.nju.edu.cn>
2019-02-12 11:18:41 +0800

    using redux-thunk

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
-import { Input, Button, List } from 'antd';
 import store from './store';
-import { getInputChangeAction, getAddItemAction, getDeleteItemAction } from './store/actionCreators';
+import { getInputChangeAction, getAddItemAction, getDeleteItemAction, getTodoList } from './store/actionCreators';
+import TodoListUI from './TodoListUI';
 
 class TodoList extends Component {
 
@@ -12,30 +12,24 @@ class TodoList extends Component {
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleStoreChange = this.handleStoreChange.bind(this);
     this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.handleItemDelete = this.handleItemDelete.bind(this);
     store.subscribe(this.handleStoreChange);
   }
   render() {
     return (
-      <div style={{marginTop: '10px', marginLeft: '10px'}}>
-        <div>
-          <Input 
-            value={this.state.inputValue} 
-            placeholder='todo info' 
-            style={{width: '300px', marginRight: '10px'}}
-            onChange={this.handleInputChange}
-          />
-          <Button type="primary" onClick={this.handleButtonClick}>Submit</Button>
-        </div>
-        <List
-          style={{marginTop: '10px', width: '300px'}}
-          bordered
-          dataSource={this.state.list}
-          renderItem={(item, index) => (
-            <List.Item onClick={this.handleItemDelete.bind(this, index)}>{item}</List.Item>
-          )}
-        />
-      </div>
-    )
+      <TodoListUI 
+        inputValue={this.state.inputValue}
+        list={this.state.list}
+        handleInputChange={this.handleInputChange}
+        handleButtonClick={this.handleButtonClick}
+        handleItemDelete={this.handleItemDelete}
+      />
+    );
+  }
+
+  componentDidMount() {
+    const action = getTodoList();
+    store.dispatch(action);
   }
 
   handleInputChange(e) {
